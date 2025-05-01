04965822e66b762d7acc47fa91d4bc2f37f174d3
Subhraneel Talukdar <subhraneeltalukdar2004jan@gmail.com>
2025-05-02 00:06:34 +0530

    re-adding codes

---
+++ hill-climbing.py
@@ -0,0 +1,26 @@
+eq = input("Enter the equation in terms of x (x**2 - 4*x + 4): ")
+initial_x = float(input("Enter the initial value of x: "))
+
+def objective_function(x):
+    return eval(eq)
+
+def hill_climb(x):
+    step_size = 0.01
+    max_iterations = 1000
+
+    for _ in range(max_iterations):
+        left = x - step_size
+        right = x + step_size
+
+        if objective_function(left) > objective_function(x):
+            x = left
+        elif objective_function(right) > objective_function(x):
+            x = right
+        else:
+            break
+
+    return x, objective_function(x)
+
+best_x, best_value = hill_climb(initial_x)
+print("Local maximum at x =", round(best_x, 4))
+print("Maximum value =", round(best_value, 4))
